51fe30b18e15110695a11f647b4cff9792b7a1ff
Jed Arno <jedarno99@gmail.com>
2022-02-17 16:28:30 +0000

    Vectorised matrix construction for minor
    gives small improvement to efficiency

--- markov_model.py
+++ markov_model.py
@@ -1,29 +1,22 @@
 import numpy as np
 
-b = 1000 #The intitial upper bound for X_1
-
 def never_increase(b):
   #Construct transition matrix P
   P = np.empty([b,b])
 
-  for i in range(b):
-    for j in range(b):
+  for j in range(b):
+    P[:,j] = 1/(b - j)
 
-      if i < j:
-        P[i,j] = 0
-
-      else:
-        P[i,j] = 1/(b-j)
-
+  P = np.tril(P)
   P = P.T
-  
-  #Construct Q, the transition state for moving from one transient state to an other
-  Q = P[:-1, :-1]
-
+  print(P)
+  Q = P[:-1, :-1] #Construct Q, the transition matrix for moving between transient states
+  print(Q)
   #Calculate N, the matrix of the average number of visits between two transient nodes
   I = np.identity(Q.shape[0])
   N = I - Q
   N = np.linalg.inv(N)
+  print(N)
 
   return(sum(N[0,:]))
 
