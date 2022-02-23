fc9841e32a19f6ed6e7d525e1a90a9c47f96cf61
Jed Arno <jedarno99@gmail.com>
2022-02-23 12:09:44 +0000

    Added small documentation to function
    Just to explain return type and parameter meaning

--- markov_model.py
+++ markov_model.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+"""
+Function to return expected number of steps before reaching 1. Paramater b
+is the upper bound. Returns a float.
+"""
 def never_increase(b):
   #Construct transition matrix P
   P = np.empty([b,b])
@@ -9,13 +13,14 @@ def never_increase(b):
 
   P = np.tril(P)
   P = P.T
+  print(P)
   Q = P[:-1, :-1] #Construct Q, the transition matrix for moving between transient states
-  
+  print(Q)
   #Calculate N, the matrix of the average number of visits between two transient nodes
   I = np.identity(Q.shape[0])
   N = I - Q
   N = np.linalg.inv(N)
-
+  print(N)
   return(sum(N[0,:]))
 
 
